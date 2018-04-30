9579dd489def3dd370783a2449362996acae24ec
ngreenlaw <greenlaw_nathan@hotmail.com>
2018-04-30 00:16:45 -0700

    Update To Files
    This is a big update to the files I have made. Cleaned up and added a
    lot to the main escapeTheTomb file.

--- egyptclass.py
+++ egyptclass.py
@@ -2,18 +2,8 @@
 
 #This is the item class in the game
 class Item:
-	id = -1; #id of object in game
 	name = ""; #name of item
 	description = ""; #description of item
-	type = ""; #Type of item, eventually change to having a lookup table, ex: 1 = Light, 2 = Weapon
-	usage = []; #The features that items can interact with
-	
-	def getId(self):
-		return self.id;
-	
-	def setId(self, id_value):
-		self.id = id_value;
-		return 0;
 	
 	def getName(self):
 		return self.name;
@@ -30,44 +20,33 @@ class Item:
 		self.description = desc_value;
 		return 0;
 		
-	def getUsage(self):
-		return self.usage;
-		
-	def setUsage(self, usage_value):
-		self.usage = usage_value;
-		return 0;
-		
-	def getType(self):
-		return self.type;
-		
-	def setType(self, type_value):
-		self.type = type_value;
+	def displayDescription(self):
+		print(self.getDescription());
 		return 0;
 		
 	#Interaction Functions
-	
-	def __init__(self, iden, na, desc, ty):
-		self.setId(iden);
+	def useItem(self, works, feature_tried):
+		result = "You used the " + self.getName() + " on the " + feature_tried;
+		if works == True:
+			result = result + " and it worked!"
+			print(result);
+			return 0;
+		else:
+			result = result + " and nothing happened.";
+			print(result);
+			return 1;
+			
+	def __init__(self, na, desc):
 		self.setName(na);
-		self.setType(ty);
 		self.setDescription(desc);
 
 #This is the feature class in the game
 class Feature:
-	id = -1; #id of feature throughout the game
 	name = ""; #name of the feature
 	description = ""; #description of the feature
-	modified_description = ""; #description to of the feature when modified
-	type = ""; #Type of feature ex: statue, door
-	room_location = None; #the room that the feature is in
-	usage = []; #the types of items tha can interact with it ex: weapon, key
-	
-	def getId(self):
-		return self.id;
-	
-	def setId(self, id_value):
-		self.id = id_value;
-		return 0;
+	modified_description = ""; #description of the feature when modified
+	modified_description_bool = False; # Tells whether or not to use the modified description
+	usage = []; # The id of the item that can interact with it
 	
 	def getName(self):
 		return self.name;
@@ -103,50 +82,47 @@ class Feature:
 	def setUsage(self, usage_value):
 		self.usage = usage_value;
 		return 0;
-		
-	def getType(self):
-		return self.type;
-		
-	def setType(self, type_value):
-		self.type = type_value;
-		return 0;
-		
-	def getRoomLocation(self):
-		return self.room_location;
-		
-	def setRoomLocation(self, room_location_value):
-		self.room_location = room_location_value;
-		return 0;
 
-	#interaction functions
-	
-	def __init__(self, iden, na, desc, md, ty):
-		self.setId(iden);
+	def displayDescription(self):
+		if self.modified_description_bool == False:
+			print(self.getDescription());
+		else:
+			print(self.getModifiedDescription());
+		
+	#interaction function, returns 0 if successful and 1 if not successful
+	def interactWith(self, item_tried):
+		able_to_use = False;
+		item_tried_name = item_tried.getName();
+		if item_tried_name in self.usage:
+			able_to_use = True;
+		result = item_tried.useItem(able_to_use, self.getName());
+		if result == 0:
+			print(self.getModifiedDescription());
+			self.modified_description_bool = True;
+			return 0;
+		return 1;
+		
+	def __init__(self, na, desc, md):
 		self.setDescription(desc);
 		self.setName(na);
 		self.setModifiedDescription(md);
-		self.setType(ty);
 
 #This is the Room class in the game, it is the basis of the game, fix to match the rest of the classes with set/get functions
 class Room:
-	id = -1; # id for the room based on all things in the game, unique
 	long_description = ""; # Long description for when entering the room for the first time
 	short_description = ""; # Short Description for when entering the room every subsequent time
 	modified_description = short_description; # Modified description based on actions
 	additional_descriptions = []; # A list of additional descriptions based on what needs to be added
 	modified_description_bool = False; # Tells whether or not to use the modified description
-	items = []; # List of item objects in the room
-	features = []; # List of feature objects in the room
+	items = []; # List of item objects in the room, ids of the items
+	features = []; # List of feature objects in the room, ids of the features
 	visited = False; # Boolean to determine if the room has been visited
+	locked = False; #Boolean to determine if the room is locked, needs a puzzle or key to open it
 	north_room = None; # room to the north of this room, none if there is not one
 	south_room = None; # room to the south of this room, none if there is not one
 	east_room = None; # room to the east of this room, none if there is not one
 	west_room = None; # room to the west of this room, none if there is not one
 	
-	#Function that returns the id of the room
-	def getId(self):
-		return self.id;
-		
 	def getName(self):
 		return self.name;
 		
@@ -253,6 +229,14 @@ class Room:
 		self.visited = visited_value;
 		return 0;
 		
+	#Functions to get and set the locked variable
+	def getLocked(self):
+		return self.locked;
+	
+	def setLocked(self, locked_value):
+		self.locked = locked_value;
+		return 0;
+		
 	#Function to display the correct description, returns the string for the game to output
 	def displayDescription(self):
 		if self.getVisited() == False:
@@ -264,8 +248,7 @@ class Room:
 				return self.getModifiedDescription();
 	
 	#Function to initialize a Room object, takes id, long desc, short desc, additional desc, items, and features 
-	def __init__(self, iden, na, ld, sd, ad, it, fe):
-		self.id = iden; #change all these to set functions and finish adding set functions to rooms
+	def __init__(self, na, ld, sd, ad, it, fe, lk):
 		self.setName(na);
 		self.long_description = ld;
 		self.short_description = sd;
@@ -275,22 +258,18 @@ class Room:
 		self.items = it;
 		self.features = fe;
 		self.visited = False;
+		self.setLocked(lk);
 
 #This is the game manager class in the game
 class GameManager:
-	id = -1; #id of game manager
 	turn_count = -1; # turn count remaining
 	current_room = None; # current room the game is in
-	room_to_north = None; # the room to the north of the current room, none if there is not one
-	room_to_south = None; # the room to the south of the current room, none if there is not one
-	room_to_east = None; # the room to the east of the current room, none if there is not one
-	room_to_west = None;# the room to the west of the current room, none if there is not one
-	
-	def getId(self):
-		return self.id;
-	
-	def setId(self, id_value):
-		self.id = id_value;
+		
+	def getTurnCount(self):
+		return self.turn_count;
+		
+	def setTurnCount(self, turn_count_value):
+		self.turn_count = turn_count_value;
 		return 0;
 		
 	def getCurrentRoom(self):
@@ -299,68 +278,16 @@ class GameManager:
 	def setCurrentRoom(self, current_room_value):
 		self.current_room = current_room_value;
 		return 0;
-		
-	#Function that returns the north room object or none if there isn't one
-	def getNorthRoom(self):
-		return self.room_to_north;	
 	
-	#Function that sets the room to the north and returns 0
-	def setNorthRoom(self, north_room_value):
-		self.room_to_north = north_room_value
-		return 0;
-
-	#Function that returns the south room object or none if there isn't one
-	def getSouthRoom(self):
-		return self.room_to_south;	
-	
-	#Function that sets the room to the south and returns 0
-	def setSouthRoom(self, south_room_value):
-		self.room_to_south = south_room_value
-		return 0;	
-
-	#Function that returns the east room object or none if there isn't one	
-	def getEastRoom(self):
-		return self.room_to_east;	
-	
-	#Function that sets the room to the east and returns 0
-	def setEastRoom(self, east_room_value):
-		self.room_to_east = east_room_value
-		return 0;	
-
-	#Function that returns the west room object or none if there isn't one	
-	def getWestRoom(self):
-		return self.room_to_west;	
-	
-	#Function that sets the room to the west and returns 0
-	def setWestRoom(self, west_room_value):
-		self.room_to_west = west_room_value
-		return 0;			
-	
-	#Function to set all the rooms at once based on the current room
-	def setNSEWRoomsGM(self, cr):
-		self.setNorthRoom(cr.getNorthRoom());
-		self.setSouthRoom(cr.getSouthRoom());
-		self.setEastRoom(cr.getEastRoom());
-		self.setWestRoom(cr.getWestRoom());
-	
-	def __init__(self, iden, tc, cr):
-		self.id = iden;
+	def __init__(self, tc, cr):
 		self.turn_count = tc;
 		self.current_room = cr;
 		
 #This is the player class in the game
 class Player:
-	id = -1;
 	current_room = None;
 	inventory = [];
 	game_manager = None;
-
-	def getId(self):
-		return self.id;
-	
-	def setId(self, id_value):
-		self.id = id_value;
-		return 0;
 		
 	def getCurrentRoom(self):
 		return self.current_room;
@@ -407,20 +334,51 @@ class Player:
 			if item_to_use.getId() == it.getId():
 				#Process item to use using features and items interaction functions
 				return 0;
-		return 1;
+		return 1;	
 		
 	#This function handles moving rooms	
-	def moveRoom(self, room_to_move_to):
-		self.setCurrentRoom(room_to_move_to);
+	def moveRoom(self, room_to_move_to_string):
 		cr = self.getCurrentRoom()
-		print(cr.displayDescription());
-		cr.setVisited(True);
-		gm = self.getGameManager()
-		gm.setCurrentRoom(room_to_move_to)
-		gm.setNSEWRoomsGM(room_to_move_to); #gets the gameManager, sets its room to the room to move to, then sets the rooms NSEW based on the room
-		return 0;
+		room_to_north = cr.getNorthRoom();
+		room_to_south = cr.getSouthRoom();
+		room_to_east = cr.getEastRoom();
+		room_to_west = cr.getWestRoom();
+		is_room = False;
+		room_to_move_to = None;
+		
+		if room_to_move_to_string == "North":
+			if room_to_north != None:
+				is_room = True;
+				room_to_move_to = room_to_north;
+		
+		if room_to_move_to_string == "South":
+			if room_to_south != None:
+				is_room = True;
+				room_to_move_to = room_to_south;
+		
+		if room_to_move_to_string == "East":
+			if room_to_east != None:
+				is_room = True;
+				room_to_move_to = room_to_east;		
+		
+		if room_to_move_to_string == "West":
+			if room_to_west != None:
+				is_room = True;
+				room_to_move_to = room_to_west;		
+		
+		if is_room == True:
+			self.setCurrentRoom(room_to_move_to);
+			cr = self.getCurrentRoom()
+			print(cr.displayDescription());
+			cr.setVisited(True);
+			gm = self.getGameManager()
+			gm.setCurrentRoom(room_to_move_to)
+		
+		else:
+			print("There is no room in that direction");
+			return 1; #Not able to move
+		return 0; #Able to move
 	
-	def __init__(self, iden, inven, gm):
-		self.setId(iden);
+	def __init__(self, inven, gm):
 		self.setInventory(inven);
 		self.setGameManager(gm);
--- escapeTheTomb.py
+++ escapeTheTomb.py
@@ -1,41 +1,241 @@
 #This is the main file for running the game
+import sys
+import egyptclass as ec
+import json
 
-#function for main menu
+#Variables to hold things such that they are easier to edit
+ITEM_FILES_VAR = ["item1.txt", "item2.txt"]; #would like to change this to reading off of a file that contains the names of all the files
+FEATURE_FILES_VAR = ["feature1.txt", "feature2.txt"];
+ROOM_FILES_VAR = ["room1.txt", "room2.txt"];
+GAME_MANAGER_FILE_VAR = "gm.txt";
+PLAYER_FILE_VAR = "player.txt";
 
+#function for main menu, returns 0 if new game and 1 if load game, exits if exit is entered or returns 2 if it doesn't understand
+def mainMenu():
+	get_input = raw_input("Please enter if you would like to start a new game, load a game or exit: ");
+	if get_input.lower() == "new game":
+		return 0;
+	elif get_input.lower() == "load game":
+		return 1;
+	elif get_input.lower() == "exit":
+		sys.exit();
+	else:
+		print("I am not sure what you mean. Please try again.");
+		return 2;
+
+#----------------------------------------CREATE GET ITEM/FEATURE/ROOM FROM NAME FUNCTIONS AND UPDATE THE OTEHR FUNCTIONS
+		
 #function to create the items
+def createItem(files_list):
+	#FIX BASED ON WHAT THE JSON FILE IS
+	object_created = None;
+	list_of_objects = [];
+	#read the files
+	for fi in files_list:
+		f = open(fi, "r");
+		rf = f.read();
+		fj = json.loads(s);
+		
+		#create the object HERE IS WHERE TO EDIT
+		object_created = ec.Item(fj['name'], fj['desc']);
+		list_of_objects.append(object_created);
+		
+	return list_of_objects;
 
 #function to create the features
+def createFeature(files_list):
+	#FIX BASED ON WHAT THE JSON FILE IS
+	object_created = None;
+	list_of_objects = [];
+	#read the files
+	for fi in files_list:
+		f = open(fi, "r");
+		rf = f.read();
+		fj = json.loads(s);
+		
+		#create the object HERE IS WHERE TO EDIT
+		object_created = ec.Feature(fj['name'], fj['desc'], fj['descMod'], fj['usage']);
+		list_of_objects.append(object_created);
+		
+	return list_of_objects;
 
+#function to set the north, south, east and west rooms after the create function to replace the names with the room object itself
+def createRoomNSEW(roomList):
+	for r in roomList:
+		if r.getNorthRoom() == "":
+			r.setNorthRoom(None);
+		else:
+			for ro in roomList:
+				if ro.getName() == r.getNorthRoom():
+					r.setNorthRoom(ro);
+					break;
+
+		if r.getSouthRoom() == "":
+			r.setSouthRoom(None);
+		else:
+			for ro in roomList:
+				if ro.getName() == r.getSouthRoom():
+					r.setSouthRoom(ro);
+					break;
+					
+		if r.getEastRoom() == "":
+			r.setEastRoom(None);
+		else:
+			for ro in roomList:
+				if ro.getName() == r.getEastRoom():
+					r.setEastRoom(ro);
+					break;
+					
+		if r.getWestRoom() == "":
+			r.setWestRoom(None);
+		else:
+			for ro in roomList:
+				if ro.getName() == r.getWestRoom():
+					r.setWestRoom(ro);
+					break;
+					
 #function to create the rooms
-
+def createRoom(files_list):
+	#FIX BASED ON WHAT THE JSON FILE IS
+	object_created = None;
+	list_of_objects = [];
+	#read the files
+	for fi in files_list:
+		f = open(fi, "r");
+		rf = f.read();
+		fj = json.loads(s);
+		
+		#create the object HERE IS WHERE TO EDIT
+		object_created = ec.Room(fj['name'], fj['descL'], fj['descS'], fj['descAdd'], fj['items'], fj['features'], fj['locked']);
+		object_created.setNorthRoom(fj['north_room'];
+		object_created.setSouthRoom(fj['south_room'];
+		object_created.setEastRoom(fj['east_room'];
+		object_created.setWestRoom(fj['west_room'];
+		list_of_objects.append(object_created);
+		
+	return list_of_objects;
+	
 #function to create the game manager
+def createGameManager(room):
+	f = open(GAME_MANAGER_FILE_VAR, "r");
+	rf = f.read();
+	fj = json.loads(s);
+	gm = ec.GameManager(fj['turnCount'], room);
+	return gm;
 
 #function to create the player
+def createPlayer(gm, items):
+	f = open(PLAYER_FILE_VAR, "r");
+	rf = f.read();
+	fj = json.loads(s);
+	gm = ec.Player(items, gm);
+	return gm;
 
 #functions to get the input and parse it
+def getInput():
+	#FILL IN THE FUNCTIONS WITH THE NATURAL LANGUAGE PARSER OR IMPORT THE MODULE
+	#RETURNS THE TAG TO BE PROCESSED
+        print("input");
 
 #functions to resolve the action and update necessary variables
-
+def processTag(returned_tag, player):
+	#Set up the needed variables
+	cr = player.getCurrentRoom();
+	
+	#set up the items list
+	items_in_room = cr.getItems();
+	items_in_inventory = player.getInventory();
+	for i in items_in_inventory:
+		items_in_room.append(i);
+	all_items = [];
+	for it in items_in_room:
+		all_items.append("Look " + it.getName());
+	
+	#Set up the variables that will execute certain functions
+	direction = ["North", "South", "East", "West"];
+	look = "Look";
+	look_at_item = all_items;
+	
+	
+	#Process the tag based on what is returned, includes the verbs used in the game
+	
+	#Move Room if the returned Tag is a direction
+	if returned_tag in direction:
+		player.moveRoom(returned_tag);
+	
+	#Display the description if the returned tag is look
+	elif returned_tag == look:
+		cr.displayDescription();
+		
+	#Display the description if the returned tag is look item
+	elif returned_tag in look_at_item: 
+		#Search through the items list and then compare the name to the returned tag
+		for its in items_in_room:
+			if returned_tag[5:] == its.getName(): #Everything after "Look "
+				its.displayDescription();
+	
+	#elif returned_tag == 
+	#...
+	#...
+	#...
+	
+	else:
+		print("You cannot do that");
 def main():
 	
-	#Start Main Menu
-	#Process command in main menu
-	
-	#Begin creating game
-	
-	#Create items
-	#Create features
-	#Create rooms
-	#Create game manager
-	#Create player
+	#Start Main Menu, until a result of save, load or exit is given, repeat asking for input
+	get_main_menu_result = 2;
+	while get_main_menu_result == 2:
+		get_main_menu_result = mainMenu();
+		
+	#Process command from main menu
+	#new game issued	
+	if get_main_menu_result == 0:
+		#Begin creating game
+		
+		#Create items for the whole game
+		items = createItem(ITEM_FILES_VAR);
+		
+		#Create features for the whole game
+		features = createFeature(FEATURE_FILES_VAR);
+		
+		#Create rooms for the whole game
+		rooms = createRoom(ROOM_FILES_VAR);
+		createRoomNSEW(rooms);
+		
+		#Create game manager
+		gameManager = createGameManager(rooms[0]);
+		
+		#Create player
+		starting_inventory = ["Torch"];
+		player = createPlayer(gameManager, starting_inventory); 
+		
+	#Load game	
+	elif get_main_menu_result == 1:
+		#BEGIN GAME LOADING
+		print("Loading Game");
+                
+	else:
+		print("DEBUG - SOMETHING WENT WRONG IN THE MAIN MENU, THIS SHOULD NOT HAPPEN");
 	
 	#Begin Game
-	
 	#While the turn count is > 0
+	turns_left = gameManager.getTurnCount();
+	while turns_left > 0:
 		#Display description
+		cr = player.getCurrentRoom();
+		cr.displayDescription();
+		
 		#Get input and resolve actions
+		input_given = getInput();
+		
 		#Display result / if error then do not print result
-
+		processTag(input_given, player);
+		
+		#Reduce turn count
+		gameManager.setTurnCount(turns_left-1);
+		turns_left = gameManager.getTurnCount();
+		
 #run the program
 if __name__ == "__main__":
 	main();
