f8889896529b6aabaf6cc5c47ee083ff7f5790ab
Colpack20 <Colpack20@gmail.com>
2018-05-21 00:59:13 -0400

    Update rooms.py

--- rooms.py
+++ rooms.py
@@ -9,6 +9,11 @@ room7 = {}
 room8 = {}
 room9 = {}
 room10 = {}
+room11 = {}
+room12 = {}
+room13 = {}
+room14 = {}
+room15 = {}
 
 room1 = {
 	"name": "Entry",
@@ -151,7 +156,76 @@ room10 = {
 	"west_room": "Food Room",
 	"locked": "False"
 }
-
+room11 = {
+	"name": "Guard's Room", 
+	"descL": "You enter a large room. Along the stone wall at the north side of the room are 3 tapestries, the one on the left has an image of the god Set pointing a sword to the right, the one in the middle has an image of the god Osiris sitting on a throne and the one on the right has an image of the goddess Isis kneeling. Along the west wall there are 3 small empty tombs with their lids already open and along the south wall is a weapons rack that has a few spears and swords. The room has a stench of death and laying by one of the tombs is a recently deceased adventurer with his pack still on his back.",
+	"descMod": "",
+	"descS": "You are in what appears to be a Guard's Room. There are 3 tapestries along the north wall, one of Set, one of Osiris, and one of Isis. Along the south wall is a weapons rack with a sword and a spear and along the west wall is a dead adventurer.",
+	"features": ["Secret Stairs", "Decomposed Bodies", "Weapons Rack", "Adventurer Pack"],
+	"items": [],
+	"visited": "False",
+	"north_room": "Nile River",
+	"south_room": "",
+	"east_room": "Mirror Room",
+	"west_room": "",
+	"locked": "False"
+}
+room12 = {
+	"name": "Nile River", 
+	"descL": "You descend down the stairs to find a small room with what looks like a river flowing through the middle of it from west to east. To the north across the small river is a closed door. On the east and west walls on the opposite side of the river as you are two statues of creatures that look like mermaids. As you approach the river you start to hear the sound of beautiful singing which distracts you from what you are doing.",
+	"descMod": "",
+	"descS": "You are in a small room with a river flowing through the middle from west to east. On the north side of the river is a door and on opposite sides of the river are mermaid statues. You can hear beautiful singing.",
+	"features": ["River", "Siren Song","Locked Door"],
+	"items": [],
+	"visited": "False",
+	"north_room": "Chamber of Passing",
+	"south_room": "Guard's Room",
+	"east_room": "",
+	"west_room": "",
+	"locked": "False"
+}
+room13 = {
+	"name": "Chamber of Passing", 
+	"descL": "You walk into the room and it is completely pitch black. You cannot see anything.",
+	"descMod": "",
+	"descS": "You are in a completely pitch black room. You cannot see anything.",
+	"features": ["Stairs", "Hieroglyphs", "Jackal Statues"],
+	"items": [],
+	"visited": "False",
+	"north_room": "",
+	"south_room": "Nile River",
+	"east_room": "",
+	"west_room": "Sphinx's Room",
+	"locked": "False"
+}
+room14 = {
+	"name": "Sphinx's Room", 
+	"descL": "You descend down the stairs and when you enter the room a voice booms out startling you and says: There are two sisters: one gives birth to the other and she, in turn, gives birth to the first. What are they? Greeting you is a sphinx statue directly in front of you on the east wall. The sphinx is massive and takes up the majority of the room and scattered around it are tons of jeweled scarabs.",
+	"descMod": "",
+	"descS": "As you enter the room, you see a sphinx statue on the east wall. The sphinx is massive and takes up the majority of the room and scattered around it are tons of jeweled scarabs.",
+	"features": ["Sphinx", "Scarab Brooches"],
+	"items": [],
+	"visited": "False",
+	"north_room": "",
+	"south_room": "",
+	"east_room": "Chamber of Passing",
+	"west_room": "Burial Chamber",
+	"locked": "False"
+}
+room15 = {
+	"name": "Burial Chamber", 
+	"descL": "You carefully walk down the stairs and enter a large room. On the east wall in front of you is a giant unopened tomb in between two large Griffin statues. Although the room is massive it is otherwise empty. There are no other doors you see and thus appears to be a dead-end.",
+	"descMod": "",
+	"descS": "In the large room there is a giant unopened tomb in between two large Griffin statues. There are no other exits, this appears to be the end.",
+	"features": ["Ancient Artifact", "Griffins"],
+	"items": [],
+	"visited": "False",
+	"north_room": "",
+	"south_room": "",
+	"east_room": "Sphinx's Room",
+	"west_room": "",
+	"locked": "False"
+}
 s = json.dumps(room1)
 t = json.dumps(room2)
 u = json.dumps(room3)
@@ -162,6 +236,11 @@ y = json.dumps(room7)
 z = json.dumps(room8)
 a = json.dumps(room9)
 b = json.dumps(room10)
+c = json.dumps(room11)
+d = json.dumps(room12)
+e = json.dumps(room13)
+f = json.dumps(room14)
+g = json.dumps(room15)
 
 with open("room1.json","w+") as f:
 	f.write(s)
@@ -183,3 +262,13 @@ with open("room9.json","w+") as f:
 	f.write(a)
 with open("room10.json","w+") as f:
 	f.write(b)
+with open("room11.json","w+") as f:
+	f.write(c)
+with open("room12.json","w+") as f:
+	f.write(d)
+with open("room13.json","w+") as f:
+	f.write(e)
+with open("room14.json","w+") as f:
+	f.write(f)
+with open("room15.json","w+") as f:
+	f.write(g)	
