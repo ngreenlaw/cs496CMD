5793a71850b51301ecf33c6ae886b80b981a57b2
Nathan Greenlaw <greenlaw_nathan@hotmail.com>
2018-05-25 23:26:38 -0700

    Updated descriptions and fixed previous returned errors

--- rooms.py
+++ rooms.py
@@ -20,7 +20,7 @@ room1 = {
 	"descL": "The large entry way into the tomb, there are hieroglyphs on each of the walls. There is a large doorway in front of you that looks like it leads to a larger space.",
 	"descMod": "",
 	"descS": "You are in the entry of the tomb. There is a doorway in front of you.",
-	"features": ["left, hieroglyphs", "right hieroglyphs"],
+	"features": ["left hieroglyphs", "right hieroglyphs"],
 	"items": [],
 	"visited": "False",
 	"north_room": "Main Passage",
@@ -67,7 +67,7 @@ room4 = {
 	"items": [],
 	"visited": "False",
 	"north_room": "",
-	"south_room": "Antechamber 2",
+	"south_room": "Antechamber 1",
 	"east_room": "",
 	"west_room": "Treasure Room",
 	"locked": "False"
@@ -158,10 +158,10 @@ room10 = {
 }
 room11 = {
 	"name": "Guard's Room", 
-	"descL": "You enter a large room. Along the stone wall at the north side of the room are 3 tapestries, the one on the left has an image of the god Set pointing a sword to the right, the one in the middle has an image of the god Osiris sitting on a throne and the one on the right has an image of the goddess Isis kneeling. Along the west wall there are 3 small empty tombs with their lids already open and along the south wall is a weapons rack that has a few spears and swords. The room has a stench of death and laying by one of the tombs is a recently deceased adventurer with his pack still on his back.",
+	"descL": "You enter a large room. Along the stone wall at the north side of the room are 3 tapestries, the one on the left has an image of the god Set pointing a sword to the right, the one in the middle has an image of the god Ra sitting on a throne and the one on the right has an image of the goddess Isis kneeling. Along the west wall there are 3 small empty tombs with their lids already open and along the south wall is a weapons rack that has a few spears and swords. The room has a stench of death and laying by one of the tombs is a recently deceased adventurer with his pack still on his back.",
 	"descMod": "",
-	"descS": "You are in what appears to be a Guard's Room. There are 3 tapestries along the north wall, one of Set, one of Osiris, and one of Isis. Along the south wall is a weapons rack with a sword and a spear and along the west wall is a dead adventurer.",
-	"features": ["Secret Stairs", "Decomposed Bodies", "Weapons Rack", "Adventurer Pack", "tapestry of set", "tapestry of ra", "tapestry of isis"],
+	"descS": "You are in what appears to be a Guard's Room. There are 3 tapestries along the north wall, one of Set, one of Ra, and one of Isis. Along the south wall is a weapons rack with a sword and a spear and along the west wall is a dead adventurer.",
+	"features": ["tapestry of set", "tapestry of ra", "tapestry of isis", "adventurer", "weapons rack"],
 	"items": ["sword", "spear"],
 	"visited": "False",
 	"north_room": "Nile River",
@@ -175,28 +175,28 @@ room12 = {
 	"descL": "You descend down the stairs to find a small room with what looks like a river flowing through the middle of it from west to east. To the north across the small river is a closed door. On the east and west walls on the opposite side of the river as you are two statues of creatures that look like mermaids. As you approach the river you start to hear the sound of beautiful singing which distracts you from what you are doing.",
 	"descMod": "",
 	"descS": "You are in a small room with a river flowing through the middle from west to east. On the north side of the river is a door and on opposite sides of the river are mermaid statues. You can hear beautiful singing.",
-	"features": ["River", "Siren Song","Locked Door","wooden door","siren statue"],
+	"features": ["wooden door","siren statue"],
 	"items": [],
 	"visited": "False",
 	"north_room": "Chamber of Passing",
 	"south_room": "Guard's Room",
 	"east_room": "",
 	"west_room": "",
-	"locked": "False"
+	"locked": "True"
 }
 room13 = {
 	"name": "Chamber of Passing", 
 	"descL": "You walk into the room and it is completely pitch black. You cannot see anything.",
 	"descMod": "",
 	"descS": "You are in a completely pitch black room. You cannot see anything.",
-	"features": ["Stairs", "writings", "jackal statue","chimera statue"],
+	"features": ["writings", "jackal statue","chimera statue"],
 	"items": ["necklace", "brooch"],
 	"visited": "False",
 	"north_room": "",
 	"south_room": "Nile River",
 	"east_room": "",
 	"west_room": "Sphinx's Room",
-	"locked": "False"
+	"locked": "True"
 }
 room14 = {
 	"name": "Sphinx's Room", 
@@ -214,17 +214,17 @@ room14 = {
 }
 room15 = {
 	"name": "Burial Chamber", 
-	"descL": "You carefully walk down the stairs and enter a large room. On the east wall in front of you is a giant unopened tomb in between two large Griffin statues. Although the room is massive it is otherwise empty. There are no other doors you see and thus appears to be a dead-end.",
+	"descL": "You carefully walk down the stairs and enter a large room. On the east wall in front of you is a giant unopened tomb in between a large Griffin statue and an Eagle statue. Although the room is massive it is otherwise empty. There are no other doors you see and thus appears to be a dead-end.",
 	"descMod": "",
-	"descS": "In the large room there is a giant unopened tomb in between two large Griffin statues. There are no other exits, this appears to be the end.",
-	"features": ["Griffins", "large tomb", "griffin statue", "eagle statue"],
+	"descS": "In the large room there is a giant unopened tomb in between a large Griffin statue and an Eagle statue. There are no other exits, this appears to be the end.",
+	"features": ["large tomb", "griffin statue", "eagle statue"],
 	"items": ["ancient artifact"],
 	"visited": "False",
 	"north_room": "",
 	"south_room": "",
 	"east_room": "Sphinx's Room",
 	"west_room": "",
-	"locked": "False"
+	"locked": "True"
 }
 
 s = json.dumps(room1)
@@ -240,7 +240,7 @@ b = json.dumps(room10)
 c = json.dumps(room11)
 d = json.dumps(room12)
 e = json.dumps(room13)
-f = json.dumps(room14)
+i = json.dumps(room14)
 g = json.dumps(room15)
 
 with open("room1.json","w+") as f:
@@ -270,6 +270,6 @@ with open("room12.json","w+") as f:
 with open("room13.json","w+") as f:
 	f.write(e)
 with open("room14.json","w+") as f:
-	f.write(f)
+	f.write(i)
 with open("room15.json","w+") as f:
 	f.write(g)	
